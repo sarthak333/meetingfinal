c6c634cfecc73840638abb3ae452ea19b7cd1636
Sarthak333 <sarthak333jha@gmail.com>
2019-01-27 00:58:59 +0530

    final

--- app/controllers/urecords_controller.rb
+++ app/controllers/urecords_controller.rb
@@ -10,13 +10,13 @@ urec=Urecord.find_by(slot_id: params[:slot_id])
 srec=Srecord.find_by(user_id: params[:user_id])
 srec.update_column(:status, "Confirmed")
 urec.update_column(:status, "Accepted")
-    redirect_back(fallback_location: root_path)
+
 end
 def no
   urec=Urecord.find_by(slot_id: params[:slot_id])
   srec=Srecord.find_by(user_id: params[:user_id])
 srec.update_column(:status, "Declined")
 urec.update_column(:status, "Declined")
-    redirect_back(fallback_location: root_path)
+
 end
 end
