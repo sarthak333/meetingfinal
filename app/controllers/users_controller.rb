bf57e999543890994a99a268620241baf937babf
Sarthak333 <sarthak333jha@gmail.com>
2019-01-26 15:07:33 +0530

    as

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
   end
 
   def new
-    #@user = User.new
+    @user = User.new
+    render 'new'
   end
   def addSlot
     user= User.find(params[:user_id])
