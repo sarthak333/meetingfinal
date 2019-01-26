337902e60071ee5a0340c9daf0a297094bed0c1c
Sarthak333 <sarthak333jha@gmail.com>
2019-01-27 01:06:50 +0530

    fin

--- app/controllers/urecords_controller.rb
+++ app/controllers/urecords_controller.rb
@@ -10,13 +10,15 @@ urec=Urecord.find_by(slot_id: params[:slot_id])
 srec=Srecord.find_by(user_id: params[:user_id])
 srec.update_column(:status, "Confirmed")
 urec.update_column(:status, "Accepted")
-
+flash[:success] = "Accepted Invitation"
+redirect_back(fallback_location: root_path)
 end
 def no
   urec=Urecord.find_by(slot_id: params[:slot_id])
   srec=Srecord.find_by(user_id: params[:user_id])
 srec.update_column(:status, "Declined")
 urec.update_column(:status, "Declined")
-
+flash[:danger] = "Declined Invitation"
+redirect_back(fallback_location: root_path)
 end
 end
